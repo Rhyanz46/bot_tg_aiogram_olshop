71246a500492780fa76cecacf61b365ed497c33f
rhyanz46 <rianariansaputra@gmail.com>
2020-04-26 14:14:28 +0800

    db connection handler

--- database.py
+++ database.py
@@ -1,10 +1,27 @@
 import mysql.connector
 from config import DatabaseConfig
+from mysql.connector import errorcode
 
 db_conf = DatabaseConfig()
-cnx = mysql.connector.connect(
-    user=db_conf.user,
-    password=db_conf.pw,
-    host=db_conf.host,
-    database=db_conf.db
-)
+
+
+def mysql_get_mydb():
+    try:
+        cnx_inside = mysql.connector.connect(
+            user=db_conf.user,
+            password=db_conf.pw,
+            host=db_conf.host,
+            database=db_conf.db
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        cnx_inside.close()
+    return cnx_inside
+
+
+cnx = mysql_get_mydb()
